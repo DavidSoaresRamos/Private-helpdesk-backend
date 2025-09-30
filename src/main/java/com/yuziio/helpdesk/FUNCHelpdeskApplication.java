60ff5e982932784e3c5c5970c0f4fa62afdb049c
DavidRamos <davidsramos527@gmail.com>
2025-09-30 11:23:44 -0300

    Add files via upload

---
+++ src/main/java/com/yuziio/helpdesk/FUNCHelpdeskApplication.java
@@ -0,0 +1,15 @@
+package com.yuziio.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*  */
+@SpringBootApplication
+public class FUNCHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FUNCHelpdeskApplication.class, args);
+	}
+
+}
