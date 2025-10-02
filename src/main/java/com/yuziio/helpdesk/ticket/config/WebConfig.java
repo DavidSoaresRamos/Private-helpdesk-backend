5d7d19e65b0970af1df886a8490627cba7036c12
DavidSoaresRamos <clashdavid527@gmail.com>
2025-10-02 10:12:01 -0300

    Config cors

---
+++ src/main/java/com/yuziio/helpdesk/ticket/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.yuziio.helpdesk.ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // aplica em todos os endpoints
+                .allowedOrigins(
+                        "https://help-desk-para-chamados-ti.vercel.app",
+                        "https://dashboard-helpdesk.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
